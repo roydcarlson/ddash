8e60d3883bf09b9cdc0c5e26087bf8f991bd6530
Omar Metwally <omar.metwally@gmail.com>
2017-08-30 19:44:48 +0000

    Added Interface class

--- interface.py
+++ interface.py
@@ -19,35 +19,38 @@ class Interface:
     # by default
 
     def __init__(self,host='localhost',port=5001):
-        self.last_added = None
+        self.last_hash_added = None
         self.api = ipfsapi.connect(host='localhost',port=5001)
-        self.contract_address = contract_address
-        self.web3 = Web3(HTTPProvider('http://localhost:8545')
+        self.web3 = Web3(HTTPProvider('http://localhost:8545'))
         self.blockNumber = self.web3.eth.blockNumber
-        self.eth_accounts = self.web3.listAccounts
+        self.eth_accounts = self.web3.personal.listAccounts
         
-
         self.tx = {}
-        self.tx['to'] = self.contract_address
-        self.tx['from'] = "0xafc473881370c6be639ac259e6f36cdc86b6a778"  
 
-        def load_contract(self,abi=None,contract_address="0xadb16223621e10d0864ff5df8d4ff5c686ca87bb"):
+        print "Welcome to the DDASH interface manager. Please make sure you have IPFS daemon and an Ethereum client running on your machine."
 
-            if not abi:
-                t = r"""[{"constant":false,"inputs":[{"name":"_row","type":"uint256"}],"name":"get_record","outputs":[{"name":"_id","type":"uint256"},{"name":"_ipfs_hash","type":"string"},{"name":"_description","type":"string"},{"name":"_shared_by_fingerprint","type":"string"},{"name":"_shared_with_fingerprint","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"die","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"seed","type":"uint256"}],"name":"randomGen","outputs":[{"name":"randomNumber","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_ipfs_hash","type":"string"},{"name":"_description","type":"string"},{"name":"_shared_with_fingerprint","type":"string"},{"name":"_shared_by_fingerprint","type":"string"}],"name":"push_record","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_i","type":"uint256"}],"name":"greet_omar","outputs":[{"name":"greeting","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_id","type":"uint256"},{"indexed":false,"name":"_ipfs_hash","type":"string"},{"indexed":false,"name":"_description","type":"string"},{"indexed":false,"name":"_shared_with_fingerprint","type":"string"},{"indexed":false,"name":"_shared_by_fingerprint","type":"string"}],"name":"RecordCreated","type":"event"}]"""
+    def load_contract(self,abi=None,sender_address="0xafc473881370c6be639ac259e6f36cdc86b6a778",contract_address="0xadb16223621e10d0864ff5df8d4ff5c686ca87bb"):
 
-                json_abi = json.loads(t)
-                self.contract = self.web3.eth.contract(abi=json_abi,address=contract_address)
-                if self.contract: 
-                    print "You are now interfacing with contract at address "+contract_address
+        self.tx['to'] = contract_address
+        self.tx['from'] = sender_address
+
+        if not abi:
+            t = r"""[{"constant":false,"inputs":[{"name":"_row","type":"uint256"}],"name":"get_record","outputs":[{"name":"_id","type":"uint256"},{"name":"_ipfs_hash","type":"string"},{"name":"_description","type":"string"},{"name":"_shared_by_fingerprint","type":"string"},{"name":"_shared_with_fingerprint","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"die","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"seed","type":"uint256"}],"name":"randomGen","outputs":[{"name":"randomNumber","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_ipfs_hash","type":"string"},{"name":"_description","type":"string"},{"name":"_shared_with_fingerprint","type":"string"},{"name":"_shared_by_fingerprint","type":"string"}],"name":"push_record","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_i","type":"uint256"}],"name":"greet_omar","outputs":[{"name":"greeting","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_id","type":"uint256"},{"indexed":false,"name":"_ipfs_hash","type":"string"},{"indexed":false,"name":"_description","type":"string"},{"indexed":false,"name":"_shared_with_fingerprint","type":"string"},{"indexed":false,"name":"_shared_by_fingerprint","type":"string"}],"name":"RecordCreated","type":"event"}]"""
+
+            json_abi = json.loads(t)
+            self.contract = self.web3.eth.contract(abi=json_abi,address=contract_address)
+            if self.contract: 
+                print "You are now interfacing with contract at address "+contract_address
 
 
 
-    def random():
+    def random(self):
         assert(self.contract)
+        assert(self.tx)
+
         i = randint(0,9)
 
-        return self.contract.transact(tx).randomGen(i)
+        return self.contract.transact(self.tx).randomGen(i)
 
     def heyo():
         assert(self.contract)
@@ -61,7 +64,7 @@ class Interface:
         assert(os.path.isfile(filepath))
         assert(self.api)
         
-        result = self.api.add(filepath)
+        self.last_hash_added = result = self.api.add(filepath)
         if result:
             print "'"+result['Name']+"' was uploaded to IPFS with hash:\n "+result['Hash']
             return result['Name'],result['Hash']
@@ -72,6 +75,10 @@ class Interface:
 
     def save_ipfs_hash_to_chain(self,ipfs_hash,description,recipient_id,sender_id="public"):
         assert(self.contract)
+        assert(self.tx)
+        if not self.last_hash_added: 
+            print "You must upload a file to IPFS first. Try Interface.upload(filepath)."
+            return 1
 
         tx_hash = self.contract(self.tx).push_record(ipfs_hash,description,recipient_id,sender_id)
 
